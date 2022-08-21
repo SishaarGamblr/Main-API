fdd7bfe4114e7146de6f6a08e30427bfb6f72119
Sishaar Rao <sishaar@gmail.com>
2022-08-21 14:44:53 -0700

    define a schema

---
+++ src/router.ts
@@ -0,0 +1,6 @@
+import { FastifyInstance } from "fastify";
+
+export default async function router(fastify: FastifyInstance) {
+  // fastify.register(mlbLinesController, { prefix: '/mlb'});
+  // fastify.register(nflLinesController, { prefix: '/nfl'});
+}
