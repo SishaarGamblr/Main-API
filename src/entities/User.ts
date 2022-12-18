481c8deadbac3f91eb19a659030281dd89cf31c9
Sishaar Rao <sishaar@gmail.com>
2022-12-17 22:26:21 -0500

    add initial UserResponse schema

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,25 +1,63 @@
-import { Entity, PrimaryGeneratedColumn, Column, Unique, OneToMany, OneToOne, JoinColumn } from 'typeorm';
-import { BaseEntity } from './BaseEntity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Unique,
+  OneToMany,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { BaseEntity, BasePropertiesSchema } from './BaseEntity';
 import { UsersToLeagues } from './UsersInLeagues';
 import { Wallet } from './Wallet';
+import { FastifySchema } from 'fastify';
+import { ErrorPropertiesSchema } from '../lib/errors/schema';
 
 @Entity()
 export class User extends BaseEntity {
-  protected prefix = 'user_';
+  prefix = 'user_';
 
   @Column({ nullable: false })
   name!: string;
 
-  @Column({ nullable: true, unique: true })
+  @Column({ nullable: true })
   email: string;
 
   @Column({ nullable: false, unique: true })
   phone!: string;
 
   @OneToMany(() => UsersToLeagues, (userToLeague) => userToLeague.userId)
-  userToLeagues!: UsersToLeagues[]
+  userToLeagues!: UsersToLeagues[];
 
-  @OneToOne(() => Wallet)
+  @OneToOne(() => Wallet, (wallet) => wallet.owner, { cascade: true })
   @JoinColumn()
   wallet: Wallet;
+}
+
+export const UserResponseSchema = {
+  200: {
+    type: 'object',
+    properties: {
+      ...BasePropertiesSchema,
+      name: { type: 'string' },
+      email: { type: 'string' },
+      phone: { type: 'string' },
+    },
+  },
+  '4xx': {
+    type: 'object',
+    properties: {
+      ...ErrorPropertiesSchema,
+    },
+  },
+};
+
+export class CreateUserDTO {
+  email: string;
+  phone: string;
+  name: string;
+}
+
+export class FindOneDTO {
+  deleted?: boolean;
 }
