510806bcbe7c908f5454b0143415802f4ab64801
Sishaar Rao <sishaar@gmail.com>
2022-12-17 23:19:01 -0500

    fix configuration so that production build can be run locally

--- src/utils/database/connection.ts
+++ src/utils/database/connection.ts
@@ -5,13 +5,9 @@ const connection: DataSourceOptions = {
   type: 'postgres',
   url: Config.get<string>('database.url'),
   ssl: Config.get<boolean>('database.ssl') && { rejectUnauthorized: false },
-  entities: ['src/entities/*.ts', 'dist/entities/*{.ts,.js}'],
+  // entities: ['src/entities/*.ts', 'dist/entities/*{.ts,.js}'],
+  entities: [`${__dirname}/../../entities/*{.ts,.js}`],
   migrations: [`${__dirname}/migrations/*{.ts,.js}`],
-  // @ts-ignore
-  cli: {
-    entitiesDir: 'src/entities',
-    migrationsDir: 'src/utils/database/migrations',
-  },
 };
 
 export default new DataSource(connection);
