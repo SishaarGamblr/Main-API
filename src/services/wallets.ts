d7aef5ae9ad69fa8d6aac995a18538a6a2421f47
Sishaar Rao <sishaar@gmail.com>
2022-12-22 09:43:05 +0530

    define service for wallet entity

---
+++ src/services/wallets.ts
@@ -0,0 +1,28 @@
+import { Service } from "typedi";
+import { Wallet } from "../entities/Wallet";
+import { FindOneOptions } from "typeorm";
+import { NotFoundError } from "../lib/errors/errors";
+
+@Service()
+export class WalletsService {
+  async findOneOrFail(id: string, params?: FindOneDTO) {
+    const options: FindOneOptions<Wallet> = {
+      where: {
+        id,
+        deleted: params?.deleted ? params.deleted : false,
+      },
+    };
+
+    const wallet = await Wallet.findOne(options);
+
+    if (!wallet) {
+      throw new NotFoundError('Wallet');
+    }
+    
+    return wallet;
+  }
+}
+
+interface FindOneDTO {
+  deleted?: boolean;
+}
