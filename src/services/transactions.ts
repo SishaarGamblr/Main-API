86d3d0fe62dda879f496ea52b995fe29f2afa4d4
Sishaar Rao <sishaar@gmail.com>
2022-12-21 22:31:49 -0800

    Migrate transactions wallet functionality to service (#13)

--- src/services/transactions.ts
+++ src/services/transactions.ts
@@ -1,6 +1,9 @@
-import { Service } from 'typedi';
+import Container, { Service } from 'typedi';
 import { Transaction } from '../entities/Transaction';
-import { EntityManager, FindOneOptions } from 'typeorm';
+import { EntityNotFoundError, FindOneOptions } from 'typeorm';
+import connection from '../utils/database/connection';
+import { Wallet } from '../entities/Wallet';
+import { InsufficientBalanceError, NotFoundError } from '../lib/errors/errors';
 
 @Service()
 export class TransactionsService {
@@ -15,15 +18,69 @@ export class TransactionsService {
     return await Transaction.findOne(options);
   }
 
-  TRANSACTION = {
-    async create(tx: EntityManager, options: CreateTransactionDTO) {
-      const transaction = Transaction.create({
-        amount: options.amount,
-        from: { id: options.fromId },
-        to: { id: options.toId },
-      });
+  async findOneOrFail(id: string, params?: FindOneDTO) {
+    const options: FindOneOptions<Transaction> = {
+      where: {
+        id,
+        deleted: params?.deleted ? params.deleted : false,
+      },
+    };
 
-      return await tx.save(transaction);
+    return await Transaction.findOneOrFail(options).catch((err) => {
+      if (err instanceof EntityNotFoundError) {
+        throw new NotFoundError('Transaction');
+      }
+      throw err;
+    });
+  }
+
+  TRANSACTION = {
+    async create(params: CreateTransactionDTO): Promise<Transaction> {
+      let transaction: Transaction;
+
+      return await connection
+        .transaction(async (trx) => {
+          // Acquire lock on both wallets via `SELECT FOR UPDATE...`
+          const [from, to] = await Promise.all([
+            trx.findOneOrFail(Wallet, {
+              where: { id: params.fromId },
+              lock: { mode: 'pessimistic_write' },
+            }),
+            trx.findOneOrFail(Wallet, {
+              where: { id: params.toId },
+              lock: { mode: 'pessimistic_write' },
+            }),
+          ]);
+
+          // Verify balance in From Wallet
+          if (from.balance < params.amount) {
+            throw new InsufficientBalanceError();
+          }
+
+          // Create transaction record
+          transaction = Transaction.create({
+            amount: params.amount,
+            from: { id: params.fromId },
+            to: { id: params.toId },
+          });
+
+          // Deduct/Add amount to wallet balances, save transaction
+          await Promise.all([
+            trx.update(Wallet, from.id, {
+              balance: Number(from.balance - params.amount),
+            }),
+            trx.update(Wallet, to.id, {
+              balance: Number(to.balance + params.amount),
+            }),
+            trx.save(transaction),
+          ]);
+          // COMMIT
+        })
+        .then(async () => {
+          return Container.get(TransactionsService).findOneOrFail(
+            transaction.id
+          );
+        });
     },
   };
 }
