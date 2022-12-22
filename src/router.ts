84b55ede7ff93e0842efac29f62902116e35f377
Sishaar Rao <sishaar@gmail.com>
2022-12-22 09:44:15 +0530

    enable controlelr for transactions endpoint

--- src/router.ts
+++ src/router.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from 'fastify';
 
 import users from './controllers/users';
 import leagues from './controllers/leagues';
+import transactions from './controllers/transactions';
 
 export default async function router(fastify: FastifyInstance) {
   fastify.register(users, { prefix: '/users' });
   fastify.register(leagues, { prefix: '/leagues' });
+  fastify.register(transactions, { prefix: '/transactions' });
 }
