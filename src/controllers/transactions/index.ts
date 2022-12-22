abbe7e0e84caa7130be7fc8b7d5291b482e402dd
Sishaar Rao <sishaar@gmail.com>
2022-12-22 11:53:22 +0530

    transaction should perform wallet operations within the service, not the controller

--- src/controllers/transactions/index.ts
+++ src/controllers/transactions/index.ts
@@ -1,14 +1,8 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import * as Schemas from './schemas';
 import Container from 'typedi';
-import {
-  InsufficientBalanceError,
-  NotFoundError,
-} from '../../lib/errors/errors';
+import { NotFoundError } from '../../lib/errors/errors';
 import { TransactionsService } from '../../services/transactions';
-import connection from '../../utils/database/connection';
-import { Wallet } from '../../entities/Wallet';
-import { Transaction } from '../../entities/Transaction';
 
 export default async (fastify: FastifyInstance) => {
   fastify.get(
@@ -42,51 +36,17 @@ export default async (fastify: FastifyInstance) => {
     ) {
       const transactionsService = Container.get(TransactionsService);
 
-      let transaction: Transaction;
-
-      await connection
-        .transaction(async (tx) => {
-          // Acquire lock on both wallets via `SELECT FOR UPDATE...`
-          const [from, to] = await Promise.all([
-            tx.findOneOrFail(Wallet, {
-              where: { id: request.body.fromId },
-              lock: { mode: 'pessimistic_write' },
-            }),
-            tx.findOneOrFail(Wallet, {
-              where: { id: request.body.toId },
-              lock: { mode: 'pessimistic_write' },
-            }),
-          ]);
-
-          // Verify balance in From Wallet
-          if (from.balance < request.body.amount) {
-            throw new InsufficientBalanceError();
-          }
-
-          // Deduct/Add amount to wallet balances
-          await Promise.all([
-            tx.update(Wallet, from.id, {
-              balance: Number(from.balance - request.body.amount),
-            }),
-            tx.update(Wallet, to.id, {
-              balance: Number(to.balance + request.body.amount),
-            }),
-          ]);
-
-          // Create transaction record
-          transaction = await transactionsService.TRANSACTION.create(tx, {
-            amount: request.body.amount,
-            fromId: request.body.fromId,
-            toId: request.body.toId,
-          });
-          // COMMIT
-        })
-        .catch((err) => {
-          reply.send(err);
-        })
-        .then(() => {
-          reply.status(200).send(transaction);
+      try {
+        const transaction = await transactionsService.TRANSACTION.create({
+          amount: request.body.amount,
+          fromId: request.body.fromId,
+          toId: request.body.toId,
         });
+
+        reply.status(200).send(transaction);
+      } catch (err) {
+        reply.send(err);
+      }
     }
   );
 };
