84bab55fae69bf70698d94bbcff8950336b8cc6d
Sishaar Rao <sishaar@gmail.com>
2023-01-12 21:06:03 -0800

    register plugins and login page

--- src/router.ts
+++ src/router.ts
@@ -4,10 +4,15 @@ import users from './controllers/users';
 import leagues from './controllers/leagues';
 import transactions from './controllers/transactions';
 import login from './controllers/login';
+import cookie from './plugins/cookie';
+import jwt from './plugins/jwt';
 
 export default async function router(fastify: FastifyInstance) {
+  fastify.register(cookie);
+  fastify.register(jwt);
+
+  fastify.register(login);
   fastify.register(users, { prefix: '/users' });
   fastify.register(leagues, { prefix: '/leagues' });
   fastify.register(transactions, { prefix: '/transactions' });
-  fastify.register(login);
 }
