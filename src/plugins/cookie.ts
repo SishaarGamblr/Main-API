87f198d819b87afc3bd47af33c65368e4ade2e5f
Sishaar Rao <sishaar@gmail.com>
2023-01-12 21:05:41 -0800

    define cookie plugin

--- src/plugins/cookie.ts
+++ src/plugins/cookie.ts
@@ -1,14 +1,14 @@
 import fp from 'fastify-plugin';
 import Config from 'config';
-import Cookies from '@fastify/cookie'
+import cookie from '@fastify/cookie'
 
 import { FastifyError, FastifyInstance, FastifyPluginOptions } from 'fastify';
 
 export default fp((fastify: FastifyInstance, _options: FastifyPluginOptions, done: (error?: FastifyError) => void): void => {
-  fastify.register(Cookies, {
-    secret: Config.get<string>('security.Cookie_secret')
+  fastify.register(cookie, {
+    secret: Config.get<string>('security.Cookie_secret'),
+    parseOptions: {}
   });
 
   done();
 })
-
