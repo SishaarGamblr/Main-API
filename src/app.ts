aead78efaacd9af0b1a0fd38d47b634bcc2e78c1
Sishaar Rao <sishaar@gmail.com>
2023-01-12 21:04:33 -0800

    ensure connection is in correct state before running operation

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,13 @@
 import fastify from 'fastify';
 import router from './router';
-import autoLoad from '@fastify/autoload';
-import { dirname, join } from 'path';
 
 const server = fastify({
   // Logger only for production
   logger: !['development', 'test', 'ci'].includes(String(process.env.NODE_ENV)),
+  connectionTimeout: 10000
 });
 
 // Middleware: Router
 server.register(router);
-
-// Autoload plugins
-server.register(autoLoad, {
-  dir: join(dirname(__filename), 'plugins')
-});
 
 export default server;
