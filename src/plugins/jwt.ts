1ecf63fb8b93c12fd9359da75c885d74d66cf382
Sishaar Rao <sishaar@gmail.com>
2023-01-12 21:05:50 -0800

    define jwt plugin

--- src/plugins/jwt.ts
+++ src/plugins/jwt.ts
@@ -4,12 +4,29 @@ import JWT from '@fastify/jwt'
 
 import { FastifyError, FastifyInstance, FastifyPluginOptions, FastifyReply, FastifyRequest } from 'fastify';
 
+declare module 'fastify' {
+  interface FastifyInstance {
+    authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
+  }
+}
+
 export default fp((fastify: FastifyInstance, _options: FastifyPluginOptions, done: (error?: FastifyError) => void): void => {
   fastify.register(JWT, {
-    secret: Config.get<string>('security.JWT_secret')
+    secret: Config.get<string>('security.JWT_secret'),
+    cookie: {
+      cookieName: 'refreshToken',
+      signed: false
+    },
+    sign: {
+      expiresIn: '90d'
+    }
   });
 
   fastify.decorate('authenticate', async function (request: FastifyRequest, reply: FastifyReply) {
+    if (['development', 'test'].includes(String(process.env.NODE_ENV)) && request.headers.authorization === 'Bearer test') {
+      return;
+    }
+
     try {
       await request.jwtVerify()
     } catch (err) {
